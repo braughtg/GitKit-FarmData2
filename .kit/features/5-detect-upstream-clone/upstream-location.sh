6aae318235a24a1c3df8effd3821670d3bfb9bbc
kit <kit@example.com>
2022-07-07 13:24:30 +0000

    build(kit): install features

---
+++ .kit/features/5-detect-upstream-clone/upstream-location.sh
@@ -0,0 +1,9 @@
+SCRIPT_DIR="$( cd -- "$( dirname -- "${BASH_SOURCE[0]:-$0}"; )" &> /dev/null && pwd 2> /dev/null; )";
+
+save-upstream-location() {
+    echo "${1}" > "${SCRIPT_DIR}/upstream-location.txt"
+}
+
+load-upstream-location() {
+    cat "${SCRIPT_DIR}/upstream-location.txt"
+}
---
+++ .kit/features/5-prevent-merge-to-main/install-into-client.bash
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+## This script is called once after the instance is cloned by a KitClient.
+## It runs in the clone of the instance in a KitClient.
+
+SCRIPT_DIR="$( cd -- "$( dirname -- "${BASH-SOURCE[0]:-$0}"; )" &> /dev/null && pwd 2> /dev/null; )";
+PROJECT_DIR="$(git rev-parse --show-toplevel)"
+
+cp "$SCRIPT_DIR/prevent-merge-to-main.bash" "$PROJECT_DIR/.git/hooks/pre-merge-commit"
+chmod +x "$PROJECT_DIR/.git/hooks/pre-merge-commit"
